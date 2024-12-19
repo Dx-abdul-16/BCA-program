1cc0fb0f28ac08ee7451ac1c7a9170bff0ae3ad5
Abdul Waheed A <abdulwaheeda638@gmail.com>
2024-12-19 10:12:26 +0530

    Add files via upload

---
+++ BCA-program/js/day1.js
@@ -0,0 +1,25 @@
+//variableName = value
+var myname = "DXAC";
+var myAge = 7;
+console.log("My Name is:", myname);
+console.log(typeof myname);
+console.log("My Number Year:", myAge);
+console.log(typeof myAge);
+
+let a = 20;
+let b = 33;
+let sum = a + b;
+console.log("Anwer is:", sum);
+let myname = "";
+let myAge = "";
+let myhight = "";
+let mystaus = "";
+let maritalstatus;
+let freshvariable = null;
+
+let course = "";
+let user = {
+  name: "smart",
+  age: 55,
+  dept: "BCA",
+};
